11000cc52696bad3e90584366c7fe878b20a30af
Hjordten <andr314e@stud.kea.dk>
2024-05-08 09:46:42 +0200

    .

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
-@app.before_first_request
+@app.before_request
 def create_table():
     db.create_all()
 
